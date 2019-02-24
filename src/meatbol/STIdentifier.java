5096af060b09c8a5fc7e0fa7336deabbab2fae80
Gregory Pugh <jalis43@yahoo.com>
2019-02-24 13:47:03 -0600

    -formatted and added code comments for everything.  just "beautifying". Only excluded SymbolTable as  changes anticipated for get and put functions.

--- src/meatbol/STIdentifier.java
+++ src/meatbol/STIdentifier.java
@@ -1,33 +1,43 @@
 package meatbol;
 
-public class STIdentifier extends STEntry {
-    /** Primitive type of the identifier. For homogeneous arrays
-     * this is the underlying primitive type of each value.
-     * <p>
-     * Current types include: INT, FLOAT, STRING, BOOL, DATE */
-    //TODO: might need to add a default type for more complex
-    //data structures.
+/** Symbol table entry for identifiers
+ * <p>
+ * Subclass of STEntry for identity entry types.
+ *
+ * @author Mason Pohler
+ * @author Gregory (modified ?)
+ * @author Reviewed by Riley Marfin, Mason Pohler, and Gregory Pugh
+ */
+public class STIdentifier extends STEntry
+{
+    /** Primitive type of the identifier. For homogeneous arrays this is the
+     * underlying primitive type of each value. */
+    // TODO: might need to add a default type for more complex
+    // data structures.
     SubClassif declareType;
-
-    /** Structure of the identifier.
-     * <p>
-     * Current types include: PRIMITIVE, FIXED, UNBOUNDED */
+    /** Structure of the identifier. */
     SubClassif structure;
-
-    /** How the identifier is passed as a parameter
-     * <p>
-     * Current types include: NOT, REFERENCE, VALUE */
+    /** How the identifier is passed as a parameter */
     SubClassif paramType;
-
     /** Indicates the location of the base address of the identifier
      * <p>
-     * Use 0 for local and 99 for global. In all other cases, this indicates
-     * how many functions above this one to go to reach this identifier's
-     * stack address.
+     * Use 0 for local and 99 for global. In all other cases, this indicates how
+     * many functions above this one to go to reach this identifier's stack
+     * address.
      */
     int nonlocal;
 
-    public STIdentifier(String symbol, Classif primClassif, SubClassif type, SubClassif struct, SubClassif param, int address){
+    /** Constructor
+     * * <p>
+     * Declare types are INTEGER, FLOAT, BOOLEAN, STRING, DATE
+     * Structure types are PRIMITIVE, FIXED, UNBOUNDED
+     * Parameter types are NOT, REFERENCE, VALUE
+     *
+     * @author Mason Pohler
+     */
+    public STIdentifier(String symbol, Classif primClassif, SubClassif type
+            , SubClassif struct, SubClassif param, int address)
+    {
         super(symbol, primClassif);
         this.declareType = type;
         this.structure = struct;
@@ -35,13 +45,34 @@ public class STIdentifier extends STEntry {
         this.nonlocal = address;
     }
 
-    public STIdentifier copy(STIdentifier other){
-        return new STIdentifier(other.symbol, other.primClassif, other.declareType, other.structure
-                , other.paramType, other.nonlocal);
+    /** Creates a deep copy of a STIdentifier entry
+     * <p>
+     * Used to avoid unintentional changes when passing by reference.
+     *
+     * @param other
+     * 			The entry from which to make a copy
+     *
+     * @return copy of the original STFunction
+     *
+     * @author Gregory Pugh
+     */
+    public STIdentifier copy(STIdentifier other)
+    {
+        return new STIdentifier(other.symbol, other.primClassif, other.declareType
+                , other.structure, other.paramType, other.nonlocal);
     }
 
-    public String toString(){
-        return (symbol + " " + primClassif.toString() + " " + declareType.toString()+ " " + structure.toString()
-                + " " + paramType.toString() + " " + nonlocal);
+    /** Prints formated data of entry.
+     * <p>
+     * Convenience function for printing symbol tables and error checking
+     *
+     * @author Gregory Pugh
+     */
+    @Override
+    public void printEntry()
+    {
+        System.out.printf("%-12s %-12s %-12s %-12s %-12s %d", symbol, primClassif.toString()
+                , declareType.toString(), structure.toString(), paramType.toString()
+                , nonlocal);
     }
 }
--- src/meatbol/SymbolTable.java
+++ src/meatbol/SymbolTable.java
@@ -2,69 +2,60 @@ package meatbol;
 
 import java.util.HashMap;
 
-public class SymbolTable
-{
-    private HashMap<String, STEntry> table;
+public class SymbolTable {
+	private HashMap<String, STEntry> table;
 
-    public SymbolTable()
-    {
-        this.table = new HashMap<String, STEntry>();
-    }
+	public SymbolTable() {
+		this.table = new HashMap<String, STEntry>();
+	}
 
-    public STEntry getSymbol(String symbol) throws Exception
-    {
-        if (table.containsKey(symbol))
-        {
-            return table.get(symbol);
-        }
-        else
-        {
-            throw new Exception("Symbol " + symbol + " is not within this symbol table.");
-        }
-    }
+	public STEntry getSymbol(String symbol) throws Exception {
+		if (table.containsKey(symbol)) {
+			return table.get(symbol);
+		} else {
+			throw new Exception("Symbol " + symbol + " is not within this symbol table.");
+		}
+	}
 
-    public void putSymbol(String symbol, STEntry entry)
-    {
-        table.put(symbol, entry);
-    }
+	public void putSymbol(String symbol, STEntry entry) {
+		table.put(symbol, entry);
+	}
 
+	private void initGlobal() {
+		// Control flow/end
+		table.put("def", new STControl("def", Classif.CONTROL, SubClassif.FLOW));
+		table.put("enddef", new STControl("enddef", Classif.CONTROL, SubClassif.END));
+		table.put("if", new STControl("if", Classif.CONTROL, SubClassif.FLOW));
+		table.put("endif", new STControl("endif", Classif.CONTROL, SubClassif.END));
+		table.put("else", new STControl("else", Classif.CONTROL, SubClassif.END));
+		table.put("for", new STControl("for", Classif.CONTROL, SubClassif.FLOW));
+		table.put("endfor", new STControl("endfor", Classif.CONTROL, SubClassif.END));
+		table.put("while", new STControl("while", Classif.CONTROL, SubClassif.FLOW));
+		table.put("endwhile", new STControl("endwhile", Classif.CONTROL, SubClassif.END));
 
-    private void initGlobal()
-    {
-         // Control flow/end
-         table.put("def", new STControl("def", Classif.CONTROL, SubClassif.FLOW));
-         table.put("enddef", new STControl("enddef", Classif.CONTROL, SubClassif.END));
-         table.put("if", new STControl("if", Classif.CONTROL, SubClassif.FLOW));
-         table.put("endif", new STControl("endif", Classif.CONTROL, SubClassif.END));
-         table.put("else", new STControl("else", Classif.CONTROL, SubClassif.END));
-         table.put("for", new STControl("for", Classif.CONTROL, SubClassif.FLOW));
-         table.put("endfor", new STControl("endfor", Classif.CONTROL, SubClassif.END));
-         table.put("while", new STControl("while", Classif.CONTROL, SubClassif.FLOW));
-         table.put("endwhile", new STControl("endwhile", Classif.CONTROL, SubClassif.END));
+		// Function void
+		table.put("print", new STFunction("print", Classif.FUNCTION, SubClassif.VOID, STFunction.VAR_ARGS));
 
-         // Function void
-         table.put("print", new STFunction("print", Classif.FUNCTION, SubClassif.VOID, STFunction.VAR_ARGS));
+		// Control declare
+		table.put("Int", new STControl("Int", Classif.CONTROL, SubClassif.DECLARE));
+		table.put("Float", new STControl("Float", Classif.CONTROL, SubClassif.DECLARE));
+		table.put("String", new STControl("String", Classif.CONTROL, SubClassif.DECLARE));
+		table.put("Bool", new STControl("Bool", Classif.CONTROL, SubClassif.DECLARE));
+		table.put("Date", new STControl("Date", Classif.CONTROL, SubClassif.DECLARE));
 
-         // Control declare
-         table.put("Int", new STControl("Int", Classif.CONTROL, SubClassif.DECLARE));
-         table.put("Float", new STControl("Float", Classif.CONTROL, SubClassif.DECLARE));
-         table.put("String", new STControl("String", Classif.CONTROL, SubClassif.DECLARE));
-         table.put("Bool", new STControl("Bool", Classif.CONTROL, SubClassif.DECLARE));
-         table.put("Date", new STControl("Date", Classif.CONTROL, SubClassif.DECLARE));
+		// Function int
+		// TODO: PUT CORRECT VALUE FOR ARGUMENTS
+		table.put("LENGTH", new STFunction("LENGTH", Classif.FUNCTION, SubClassif.INTEGER, 0));
+		table.put("MAXLENGTH", new STFunction("MAXLENGTH", Classif.FUNCTION, SubClassif.INTEGER, 0));
+		table.put("SPACES", new STFunction("SPACES", Classif.FUNCTION, SubClassif.INTEGER, 0));
+		table.put("ELEM", new STFunction("ELEM", Classif.FUNCTION, SubClassif.INTEGER, 0));
+		table.put("MAXELEM", new STFunction("MAXELEM", Classif.FUNCTION, SubClassif.INTEGER, 0));
 
-         // Function int
-         // TODO: PUT CORRECT VALUE FOR ARGUMENTS
-         table.put("LENGTH", new STFunction("LENGTH", Classif.FUNCTION, SubClassif.INTEGER, 0));
-         table.put("MAXLENGTH", new STFunction("MAXLENGTH", Classif.FUNCTION, SubClassif.INTEGER, 0));
-         table.put("SPACES", new STFunction("SPACES", Classif.FUNCTION, SubClassif.INTEGER, 0));
-         table.put("ELEM", new STFunction("ELEM", Classif.FUNCTION, SubClassif.INTEGER, 0));
-         table.put("MAXELEM", new STFunction("MAXELEM", Classif.FUNCTION, SubClassif.INTEGER, 0));
-
-         // Operators
-         table.put("and", new STEntry("and", Classif.OPERATOR));
-         table.put("or", new STEntry("or", Classif.OPERATOR));
-         table.put("not", new STEntry("not", Classif.OPERATOR));
-         table.put("in", new STEntry("in", Classif.OPERATOR));
-         table.put("notin", new STEntry("notin", Classif.OPERATOR));
-     }
+		// Operators
+		table.put("and", new STEntry("and", Classif.OPERATOR));
+		table.put("or", new STEntry("or", Classif.OPERATOR));
+		table.put("not", new STEntry("not", Classif.OPERATOR));
+		table.put("in", new STEntry("in", Classif.OPERATOR));
+		table.put("notin", new STEntry("notin", Classif.OPERATOR));
+	}
 }
